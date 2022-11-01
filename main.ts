c6aaf4f26b20f5316df7f70a9be7cb123f06607c
Yohan-Rodriguez <85138862+Yohan-Rodriguez@users.noreply.github.com>
2022-11-01 14:58:34 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,25 +2,19 @@ enum RadioMessage {
     message1 = 49434
 }
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 1) {
+    if (receivedNumber == 4) {
         getValue = 1
-    } else if (receivedNumber == 2) {
-        getValue = 2
-    } else if (receivedNumber == 3) {
-        getValue = 3
-    } else if (receivedNumber == 4) {
-        getValue = 4
-    } else if (receivedNumber == 5) {
-        getValue = 5
     }
+    basic.showIcon(IconNames.Happy)
     basic.pause(25000)
     getValue = 0
 })
 let getValue = 0
-radio.setGroup(100)
+radio.setGroup(50)
 basic.forever(function () {
     basic.showIcon(IconNames.No)
     pins.digitalWritePin(DigitalPin.P0, 1)
+    pins.digitalWritePin(DigitalPin.P1, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     basic.showNumber(8)
     basic.pause(500)
@@ -44,17 +38,6 @@ basic.forever(function () {
     basic.showIcon(IconNames.Square)
     basic.pause(2000)
     if (getValue == 1) {
-        basic.showString("Yohan")
-    } else if (getValue == 2) {
-        basic.showString("Andrea")
-    } else if (getValue == 3) {
-        basic.showString("Sebastian")
-    } else if (getValue == 4) {
-        basic.showString("Eymi")
-    } else if (getValue == 5) {
-        basic.showString("Mao")
-    }
-    if (getValue == 1 || getValue == 2 || getValue == 3 || getValue == 4 || getValue == 5) {
         pins.digitalWritePin(DigitalPin.P0, 0)
         pins.digitalWritePin(DigitalPin.P1, 0)
         pins.digitalWritePin(DigitalPin.P2, 1)
